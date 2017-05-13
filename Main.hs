b7b8b92aa03a12beeb8b40a485e4d8231c5f9c86
Alan Rempel <alan.rempel@gmail.com>
2017-05-13 12:36:36 -0300

    Add option parser

--- Main.hs
+++ Main.hs
@@ -8,7 +8,8 @@ import Control.Monad ( guard )
 import Data.Foldable ( for_ )
 import Data.Function ( on )
 import Data.List ( groupBy )
-import Data.Maybe ( maybe, fromMaybe, mapMaybe )
+import Data.Maybe ( maybe, mapMaybe )
+import Data.Monoid ((<>))
 import System.Exit ( die )
 
 import qualified Data.Map as Map
@@ -17,14 +18,15 @@ import qualified Data.Text as Text
 import System.Directory ( copyFileWithMetadata )
 import System.FilePath
 
+import Options.Applicative
 import Cabal.Plan
 
 main :: IO ()
 main = do
-    Options{..} <- return (Options [] Nothing Nothing Nothing)
+    Options{..} <- getOptions
 
     let distDirPrefix =
-            fromMaybe "dist-newstyle" optDistDirPrefix
+            optDistDirPrefix
 
     (plan, projectRoot) <-
         case optProjectRoot of
@@ -36,8 +38,7 @@ main = do
             Nothing ->
                 findAndDecodePlanJson
 
-    let targets = readExeTargets $ map Text.pack optTargets
-
+    let targets = readExeTargets optTargets
 
     bindir <- maybe
                   getConfiguredBindir
@@ -56,13 +57,6 @@ targetedExeComponents targets units = do
     unit@Unit{uComps}          <- units
     guard $ packageName unit == packageName target
     mapMaybe (\exeName -> Map.lookup (CompNameExe exeName) uComps) exeNames
-
-data Options = Options
-        { optTargets :: [String]
-        , optProjectRoot :: Maybe FilePath
-        , optBindir :: Maybe FilePath
-        , optDistDirPrefix :: Maybe String
-        }
 
 data ExeTarget = ExeTarget
         { pkgName :: PkgName
@@ -93,3 +87,47 @@ getConfiguredBindir :: IO FilePath
 getConfiguredBindir =
     die $ "Haven't yet figured out how to get the configured bindir.\n"
        ++ "For now you have to tell me via the `--bindir` flag."
+
+data Options = Options
+        { optTargets :: [Text]
+        , optProjectRoot :: Maybe FilePath
+        , optBindir :: Maybe FilePath
+        , optDistDirPrefix :: String
+        }
+
+getOptions :: IO Options
+getOptions = customExecParser (prefs showHelpOnError) $
+    info (helper <*> parser)
+    (  fullDesc
+    <> header "cabal-install-bins: install executables built by cabal new-build"
+    <> progDesc ( "A workaround for the lack of `cabal new-install`. cabal-install-bins reads "
+               ++ "the plan.json file produced by cabal new-build, locates the built executable "
+               ++ "files, and copies them to the install directory." )
+    )
+  where
+    parser :: Parser Options
+    parser =
+      Options
+        <$> some ( argument (Text.pack <$> str)
+            (  metavar "TARGET"
+            <> help "targets of the form pkgname:exename, or just pkgname for all executables in the package"
+            ) )
+        <*> option (Just <$> str)
+            (  long "project-root"
+            <> metavar "PATH"
+            <> help "path to the root directory of the cabal project (optional unless you're not in a subdirectory)"
+            <> value Nothing
+            )
+        <*> option (Just <$> str)
+            (  long "bindir"
+            <> metavar "DIRECTORY"
+            <> help "where to put the executables"
+            <> value Nothing
+            )
+        <*> strOption
+            (  long "distdir"
+            <> metavar "DIRECTORY"
+            <> help "the name of cabal-install's dist directory (defaults to 'dist-newstyle')"
+            <> value "dist-newstyle"
+            <> showDefault
+            )
